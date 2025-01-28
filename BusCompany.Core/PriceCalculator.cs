73a1d5eee695bf37c834a8ec7d2aee2e93e837cf
AsbjrnJacobsen <megetunikemail@gmail.com>
2025-01-28 11:45:56 +0100

    Forgot unversioned files

---
+++ BusCompany.Core/PriceCalculator.cs
@@ -0,0 +1,24 @@
+﻿namespace BusCompany.Core;
+
+public class PriceCalculator
+{
+    public static int CalculatePrice(int distanceKm)
+    {
+        const int initialFee = 2500;
+
+        if (distanceKm <= 100)
+        {
+            return initialFee + distanceKm * 10;
+        }
+        else if(distanceKm <= 500)
+        {
+            return initialFee + 100 * 10 + (distanceKm - 100) * 8;
+        }
+        else
+        {
+            return initialFee + 100 * 10 + 400 * 8 + (distanceKm - 500) * 6;
+        }
+        
+        return 2500;
+    }
+}
---
+++ BusCompany.Tests/PriceCalculatorTests.cs
@@ -0,0 +1,57 @@
+﻿using BusCompany.Core;
+using Xunit;
+
+namespace BusCompany.Tests;
+
+public class PriceCalculatorTests
+{
+    [Fact]
+    public void CalculatePrice_ShouldReturnInitialFee_WhenDistanceIsZero()
+    {
+        //Arrange
+        int distance = 0;
+        
+        //Act
+        int result = PriceCalculator.CalculatePrice(distance);
+        
+        //Assert
+        Assert.Equal(2500, result);
+    }
+
+    [Fact]
+    public void CalculatePrice_ShouldIncludeDistanceFee_WhenDistanceIsBelow100Km()
+    {
+        //Arrange
+        int distance = 50;
+        
+        //Act
+        int result = PriceCalculator.CalculatePrice(distance);
+        
+        //Assert
+        Assert.Equal(2500 + 50 * 10, result);
+    }
+
+    [Fact]
+    public void CalculatePrice_ShouldIncludeReducedRate_WhenDistanceIsBetween100And500Km()
+    {
+        //Arrange
+        int distance = 200;
+        
+        //Act
+        int result = PriceCalculator.CalculatePrice(distance);
+        
+        //Assert
+        Assert.Equal(2500 + 100 * 10 + 100 * 8, result);
+    }
+
+    [Fact]
+    public void CalculatePrice_ShouldIncludeLowestRate_WhenDistanceIsAbove500km()
+    {
+        //Arrange
+        int distance = 600;
+        //Act
+        int result = PriceCalculator.CalculatePrice(distance);
+        //Assert
+        Assert.Equal(2500 + 100 * 10 + 400 * 8 + 100 * 6, result);
+    }
+}
